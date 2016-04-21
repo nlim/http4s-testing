a0fcf30ae029b7d9aeca40b55a4ec8870cefbb0e
Nathaniel Lim <nlim@aquto.com>
2016-04-21 13:48:41 -0400

    Move stuff

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,56 +1,25 @@
-
 import scala.language.postfixOps
 
 import argonaut._, Argonaut._
 import scalaz.concurrent.Task
 import scalaz.stream._
-
-
-case class Person(id: Long, name: String)
-
-object Person {
-
-  implicit val personEncoder: EncodeJson[Person] = EncodeJson { (p: Person) =>
-    ("id"   := p.id) ->:
-    ("name" := p.name) ->:
-    jEmptyObject
-  }
-
-  val all = List(
-    Person(1, "Bob"),
-    Person(2, "Bob"),
-    Person(3, "Bob"),
-    Person(4, "Bob")
-  )
-
-  val allStream: Process[Task, Person] = Process.emitAll(all)
-
-  import scala.concurrent.duration._
-  import scalaz.concurrent.Strategy
-  val tick: Process[Task, Duration] = {
-    scalaz.stream.time.awakeEvery(100 milliseconds)(
-      Strategy.DefaultStrategy,
-      Strategy.DefaultTimeoutScheduler
-    )
-  }
-
-
-
-  val allStreamDelayed: Process[Task, Person] = {
-    allStream.zip(tick).map(t => t._1).repeat.take(1000)
-  }
-}
+import scala.language.higherKinds
 
 object DemoService extends org.http4s.argonaut.ArgonautInstances {
   import org.http4s._, org.http4s.dsl._
-  import doobie.imports._
+  //import doobie.imports._
 
   implicit def entityEncoder[A: EncodeJson]:  EntityEncoder[A] = jsonEncoderOf[A]
 
-  //def service(xa: Transactor[Task]) = HttpService {
+  def asJsonArray[T[_], A: EncodeJson](p: Process[T, A]): Process[T, String] = {
+    Process.emit("[") ++
+    p.map(_.asJson.nospaces).intersperse(",") ++
+    Process.emit("]")
+  }
+
   def service = HttpService {
     case GET -> Root / "people" =>
-      Ok(Person.allStreamDelayed)
+      Ok(asJsonArray(Person.allStreamDelayed))
   }
 }
 
@@ -59,12 +28,7 @@ object DemoService extends org.http4s.argonaut.ArgonautInstances {
 object Main {
   def main(args: Array[String]): Unit = {
     import org.http4s.server.blaze._
-
-    val server = BlazeBuilder
-      .bindHttp(9090)
-      .mountService(DemoService.service)
-      .run
-
+    val server = BlazeBuilder.bindHttp(9090).mountService(DemoService.service).run
     server.awaitShutdown()
   }
 }
